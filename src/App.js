444b39aa111746564858594e2cac89154e747963
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2021-02-19 18:25:09 -0800

    Gommet setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box, Button, Heading, Grommet } from "grommet";
+import { Notification } from "grommet-icons";
+
+const AppBar = (props) => (
+  <Box
+    tag="header"
+    direction="row"
+    align="center"
+    justify="between"
+    background="brand"
+    pad={{ left: "medium", right: "small", vertical: "small" }}
+    elevation="medium"
+    style={{ zIndex: "1" }}
+    {...props}
+  />
+);
 
 function App() {
+  const theme = {
+    global: {
+      colors: {
+        brand: "#228BE6",
+      },
+      font: {
+        family: "Roboto",
+        size: "18px",
+        height: "20px",
+      },
+    },
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grommet theme={theme}>
+      <AppBar>
+        <Heading level="3" margin="none">
+          VCR SV
+        </Heading>
+        <Button icon={<Notification />} onClick={() => {}} />
+      </AppBar>
+    </Grommet>
   );
 }
 
